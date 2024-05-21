739a1dc93aa165d9070f48a9d086d96f54de4d3e
viszya <xanthan.ex@gmail.com>
2024-05-21 08:25:21 -0700

    producthunt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
   return (
     <main className={cn("mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center", twp().wrapper)}>
       <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-green-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-green-300 hover:bg-green/50'>
-        <p className="text-sm font-semibold text-gray-700 flex flex-row ">
-          <Icons.party className="h-5 w-5 mr-2" /> PodiumAI is now beta!
-        </p>
+        <Link href="https://www.producthunt.com/products/podiumai" className="text-sm font-semibold text-gray-700 flex flex-row ">
+          <Icons.party className="h-5 w-5 mr-2" /> PodiumAI is launched on ProductHunt!
+        </Link>
       </div>
 
       {/* Hero Section */}
